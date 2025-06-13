9ece9ac7181b3700717b34fef9bfb74d326846ee
Amir Vishwas <calvintakasi@outlook.com>
2025-06-13 20:07:10 +0530

    added slogan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,12 @@ import PropTypes from "prop-types";
 function Header() {
   return (
     <>
-      <nav className="">
-        <ul className=" flex gap-15 place-content-center mt-2.5">
-          <li className=" font-semibold font text-2xl">Home</li>
-          <li className=" font-semibold font text-2xl">About</li>
-          <li className=" font-semibold font text-2xl">Support</li>
-        </ul>
-      </nav>
-      <div className="appTitle  text-center font-bold text-5xl my-10">
+      <div className="appTitle  text-center font-bold text-5xl my-6">
         TODO REACT APP
+      </div>
+
+      <div className="slogan font-semibold text-center text-2xl">
+        Turn daily intentions into action with smart reminders
       </div>
     </>
   );
