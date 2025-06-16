002b0edd6ebf9a9b9fcd61fcca4cbd86bc7aca6e
Amir Vishwas <calvintakasi@outlook.com>
2025-06-16 17:12:28 +0530

    title changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 function Header() {
   return (
     <>
-      <div className="appTitle  text-center font-bold text-5xl my-6">
-        TODO REACT APP
+      <div className="appTitle  text-center font-bold text-5xl my-6  ">
+        TODO APP
       </div>
 
       <div className="slogan font-semibold text-center text-2xl">
